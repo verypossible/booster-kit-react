379b67c0c1c4b6138322a44d11122422dab602ce
Travis Palmer <palmer.travis@gmail.com>
2016-10-11 14:04:32 -0400

    remove offensive semicolon

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const debug = _debug('app:server:express')
 const app = express()
 const paths = config.utils_paths
 
-require('events').EventEmitter.prototype._maxListeners = 100;
+require('events').EventEmitter.prototype._maxListeners = 100
 
 // This rewrites all routes requests to the root /index.html file
 // (ignoring file requests). If you want to implement universal
