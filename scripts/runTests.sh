462457ca67f5f4ee23f94ead50d21cd37ea9f699
Gabe Weaver <gabe@spartansystems.co>
2017-07-08 01:23:08 -0400

    fix webpack env selector

--- scripts/runTests.sh
+++ scripts/runTests.sh
@@ -1,13 +1,10 @@
 # This script is used by CircleCI to execute automated tests.
 
 # Build all assets
-yarn run build:test
-
-# Deploy to test environment
-yarn run deploy:test
+yarn run build
 
 # Boot a static file server
-php -S localhost:3001 -t dist/ &
+php -S localhost:3001 -t build/ &
 
 # Save the PID of the server to a variable
 APP_TEST_PID=$(echo $!)
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -2,7 +2,7 @@ require('babel-register')
 const webpackDevConfig = require('./webpack.development')
 const webpackProdConfig = require('./webpack.production')
 
-module.exports = function(env) {
+module.exports = function({ env }) {
   if (env === 'production') {
     return webpackProdConfig
   }
