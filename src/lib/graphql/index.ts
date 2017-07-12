6f494e0ba17ace183b2819ddc3d19b794dfe5330
Gabe Weaver <gabe@spartansystems.co>
2017-07-12 01:34:42 -0400

    removes unused var

--- src/lib/graphql/index.ts
+++ src/lib/graphql/index.ts
@@ -2,6 +2,6 @@ import client from './client'
 import * as mutate from './mutations'
 import * as query from './queries'
 
-export { KEY, mutate, query }
+export { mutate, query }
 
 export default client
