a421d93a34d285a61c47df9f139b708e3dbdb660
Gabe Weaver <gabe@spartansystems.co>
2017-10-13 09:48:19 -0400

    removes unused markdown helper

--- src/lib/docs/helpers/index.ts
+++ src/lib/docs/helpers/index.ts
@@ -1,7 +1,6 @@
 import compose from './compose'
 import errorBoundary from './errorBoundary'
 import getDisplayName from './getDisplayName'
-import markdown from './markdown'
 import markdownLoader from './markdownLoader'
 
-export { compose, errorBoundary, getDisplayName, markdown, markdownLoader }
+export { compose, errorBoundary, getDisplayName, markdownLoader }
